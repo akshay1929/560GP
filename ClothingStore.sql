a83b480fae27219c315107152ae223ef95529dae
Adela Rahman <arahman@users.campus>
2020-04-15 12:34:25 -0500

    added tables from adela

--- ClothingStore.sql
+++ ClothingStore.sql
@@ -1,8 +1,14 @@
 --CREATE SCHEMA Sales;
+--GO
+
+--CREATE SCHEMA Product;
+--GO
 
 DROP TABLE IF EXISTS Sales.Employees;
 DROP TABLE IF EXISTS Sales.Orders;
 DROP TABLE IF EXISTS Sales.OrderLines;
+DROP TABLE IF EXISTS Product.ProductType;
+DROP TABLE IF EXISTS Product.Inventory;
 
 CREATE TABLE Sales.Employees
 (
@@ -42,3 +48,21 @@ CREATE TABLE Sales.OrderLines
     Quantity INT NOT NULL,
     UnitPrice FLOAT NOT NULL
 )
+
+CREATE TABLE Product.Inventory
+(
+	ProductID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	SKU INT NOT NULL UNIQUE,
+	ProductName NVARCHAR(32) NOT NULL,
+	Quantity INT NOT NULL
+);
+
+CREATE TABLE Product.ProductType
+(
+	ProductType NVARCHAR(32) NOT NULL PRIMARY KEY,
+	ProductID INT NOT NULL FOREIGN KEY
+		REFERENCES Product.Inventory(ProductID),
+	[Description] NVARCHAR(128) NOT NULL,
+	Price FLOAT NOT NULL,
+	Rating FLOAT NOT NULL
+);
