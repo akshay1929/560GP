0e2e365971db08c5b25e6e1b2d9f38bee1a50075
Akshay Mhatre <akshay1999@ksu.edu>
2020-04-25 13:48:56 -0500

    Visual Studio Created

---
+++ Team 4 - Clothing Store/DataAccess/IDataDelegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public interface IDataDelegate
+    {
+        string ProcedureName { get; }
+
+        void PrepareCommand(SqlCommand command);
+    }
+}
---
+++ Team 4 - Clothing Store/DataAccess/IDataReaderDelegate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public interface IDataReaderDelegate<out T> : IDataDelegate
+    {
+        T Translate(SqlCommand command, IDataRowReader reader);
+    }
+}
