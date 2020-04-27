40303f9bd05fde4b4427d1ff2d45527b7a304658
Akshay Mhatre <akshay1999@ksu.edu>
2020-04-27 14:34:56 -0500

    Delegates added

--- Team 4 - Clothing Store/ClothingStoreData/Delegate/GetOrderDataDelegate.cs
+++ Team 4 - Clothing Store/ClothingStoreData/Delegate/GetOrderDataDelegate.cs
@@ -9,8 +9,35 @@ using System.Data.SqlClient;
 
 namespace ClothingStoreData.Delegate
 {
+   
     public class GetOrderDataDelegate : DataReaderDelegate<Orders>
     {
+        private readonly int orderID;
+        public GetOrderDataDelegate(int id)
+         : base("Users.GetMemberId")
+        {
+            this.orderID = id;
+        }
 
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("OrderID", orderID);
+        }
+
+        public override Orders Translate(SqlCommand command, IDataRowReader reader)
+        {
+            if (!reader.Read())
+                return null;
+
+            return new Orders(
+               orderID,
+               reader.GetInt32("MemberID"),
+                reader.GetInt32("EmployeeID"),
+                reader.GetInt32("ShipmentID"),
+               reader.GetDateTimeOffset("OrderDate"),
+               reader.GetString("ShipmentAddress"));
+        }
     }
 }
---
+++ Team 4 - Clothing Store/ClothingStoreData/Delegate/GetProductDataDelegate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using ClothingStoreData.Models;
+using System.Data.SqlClient;
+
+
+namespace ClothingStoreData.Delegate
+{
+    class GetProductDataDelegate : DataReaderDelegate<Product>
+    {
+        private readonly ProductType productTypeID;
+        public GetProductDataDelegate(ProductType p)
+           : base("Product.GetProduct")
+        {
+            this.productTypeID = p;
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("ProductTypeID", productTypeID);
+        }
+
+        public override Product Translate(SqlCommand command, IDataRowReader reader)
+        {
+            if (!reader.Read())
+                return null;
+
+            return new Product(
+               reader.GetString("SKU"),
+                reader.GetString("ProductName"),
+               productTypeID,
+               reader.GetInt32("Quantity"),
+                reader.GetString("Description"),
+                reader.GetFloat("Price"),
+                reader.GetFloat("Rating")
+               );
+
+        }
+    }
+}
