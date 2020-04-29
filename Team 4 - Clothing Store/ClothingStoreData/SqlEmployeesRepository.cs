7ef9260ef2e0fd1f57e4574903b2455049fd99aa
Akshay Mhatre <akshay1999@ksu.edu>
2020-04-28 23:21:33 -0500

    Sql orders and employees updates

--- Team 4 - Clothing Store/ClothingStoreData/SqlEmployeesRepository.cs
+++ Team 4 - Clothing Store/ClothingStoreData/SqlEmployeesRepository.cs
@@ -3,6 +3,8 @@ using ClothingStoreData.Delegate;
 using ClothingStoreData.Models;
 using DataAccess;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ClothingStoreData
 {
@@ -17,13 +19,85 @@ namespace ClothingStoreData
 
         public IReadOnlyList<Employees> RetrieveEmployees()
         {
-            return executor.ExecuteReader(new RetrieveEmployeesDataDelegate());
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("Employees.RetrieveEmployees", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    connection.Open();
+
+                    using (var reader = command.ExecuteReader())
+                        return TranslateEmployees(reader);
+                }
+            }
         }
 
         public Employees FetchEmployeeSalary(int employeeid)
         {
-            var d = new FetchEmployeeSalaryDataDelegate(employeeid);
-            return executor.ExecuteReader(d);
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("Employees.FetchPerson", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("EmployeeID", employeeid);
+
+                    connection.Open();
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        var employee = TranslateEmployee(reader);
+
+                        if (employee == null)
+                            throw new RecordNotFoundException(employeeid.ToString());
+
+                        return employee;
+                    }
+                }
+            }
+        }
+
+        private Employees TranslateEmployee(SqlDataReader reader)
+        {
+
+            var employeeidOrdinal = reader.GetOrdinal("EmployeeID");
+            var emailOrdinal = reader.GetOrdinal("Email");
+            var nameOrdinal = reader.GetOrdinal("Name");
+            var roleOrdinal = reader.GetOrdinal("EmployeeRole");
+            var salaryOrdinal = reader.GetOrdinal("Salary");
+
+            if (!reader.Read())
+                return null;
+            return new Employees(
+                   reader.GetInt32(employeeidOrdinal),
+                   reader.GetString(emailOrdinal),
+                   reader.GetString(nameOrdinal),
+                   reader.GetString(roleOrdinal),
+                    reader.GetInt32(salaryOrdinal)));
+            }
+
+        private IReadOnlyList<Employees> TranslateEmployees(SqlDataReader reader)
+        {
+            var employee = new List<Employees>();
+
+            var employeeidOrdinal = reader.GetOrdinal("EmployeeID");
+            var emailOrdinal = reader.GetOrdinal("Email");
+            var nameOrdinal = reader.GetOrdinal("Name");
+            var roleOrdinal = reader.GetOrdinal("EmployeeRole");
+            var salaryOrdinal = reader.GetOrdinal("Salary");
+
+            while (reader.Read())
+            {
+                employee.Add(new Employees(
+                   reader.GetInt32(employeeidOrdinal),
+                   reader.GetString(emailOrdinal),
+                   reader.GetString(nameOrdinal),
+                   reader.GetString(roleOrdinal),
+                    reader.GetInt32(salaryOrdinal)));
+            }
+
+            return employee;
         }
     }
 }
--- Team 4 - Clothing Store/ClothingStoreData/SqlOrderRepository.cs
+++ Team 4 - Clothing Store/ClothingStoreData/SqlOrderRepository.cs
@@ -3,28 +3,100 @@ using ClothingStoreData.Delegate;
 using ClothingStoreData.Models;
 using DataAccess;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ClothingStoreData
 {
     public class SqlOrderRepository : IOrderRepository
     {
-        private readonly SqlCommandExecutor executor;
+        private readonly string connectionString;
 
         public SqlOrderRepository(string connectionString)
         {
-            executor = new SqlCommandExecutor(connectionString);
+            this.connectionString = connectionString;
         }
 
         public Orders GetOrder(int id)
         {
-            var d = new GetOrderDataDelegate(id);
-            return executor.ExecuteReader(d);
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("Orders.GetOrder", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("OrderID", id);
+
+                    connection.Open();
+
+                    using (var reader = command.ExecuteReader())
+                        return TranslateOrder(reader);
+                }
+            }
         }
 
         public Orders GetOrderId(string shipmentAddress)
         {
-            var d = new GetOrderIdDataDelegate(shipmentAddress);
-            return executor.ExecuteReader(d);
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("Orders.GetOrder", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("Shipment Address", shipmentAddress);
+
+                    connection.Open();
+
+                    using (var reader = command.ExecuteReader())
+                        return TranslateOrder(reader);
+                }
+            }
+        }
+
+        private Orders TranslateOrder(SqlDataReader reader)
+        {
+            var orderIdOrdinal = reader.GetOrdinal("OrderID");
+            var memberIdOrdinal = reader.GetOrdinal("MemberID");
+            var employeeIdOrdinal = reader.GetOrdinal("EmployeeID");
+            var shipmentIdOrdinal = reader.GetOrdinal("ShipmentID");
+            var orderDateOrdinal = reader.GetOrdinal("OrderDate");
+            var shipmentAddressIdOrdinal = reader.GetOrdinal("Shipment Address");
+
+            if (!reader.Read())
+                return null;
+
+            return new Orders(
+               reader.GetInt32(orderIdOrdinal),
+                   reader.GetInt32(memberIdOrdinal),
+                   reader.GetInt32(employeeIdOrdinal),
+                   reader.GetInt32(shipmentIdOrdinal),
+                    reader.GetDateTimeOffset(orderDateOrdinal),
+                     reader.GetString(shipmentAddressIdOrdinal));
+        }
+
+        private IReadOnlyList<Orders> TranslateOrders(SqlDataReader reader)
+        {
+            var orders = new List<Orders>();
+
+            var orderIdOrdinal = reader.GetOrdinal("OrderID");
+            var memberIdOrdinal = reader.GetOrdinal("MemberID");
+            var employeeIdOrdinal = reader.GetOrdinal("EmployeeID");
+            var shipmentIdOrdinal = reader.GetOrdinal("ShipmentID");
+            var orderDateOrdinal = reader.GetOrdinal("OrderDate");
+            var shipmentAddressIdOrdinal = reader.GetOrdinal("Shipment Address");
+
+            while (reader.Read())
+            {
+                orders.Add(new Orders(
+                   reader.GetInt32(orderIdOrdinal),
+                   reader.GetInt32(memberIdOrdinal),
+                   reader.GetInt32(employeeIdOrdinal),
+                   reader.GetInt32(shipmentIdOrdinal),
+                    reader.GetDateTimeOffset(orderDateOrdinal),
+                     reader.GetString(shipmentAddressIdOrdinal)));
+            }
+
+            return orders;
         }
     }
 }
