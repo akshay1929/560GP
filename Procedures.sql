459faea61c16e601611bdd8c97fb85256d8d5f7d
Adela Rahman <arahman@users.campus>
2020-04-30 12:28:20 -0500

    all changes so far

--- Procedures.sql
+++ Procedures.sql
@@ -1,3 +1,157 @@
+/* Create Product */
+CREATE OR ALTER PROCEDURE Product.CreateProduct
+   @SKU NVARCHAR(32),
+   @ProductName NVARCHAR(64),
+   @ProductTypeId INT,
+   @Quantity INT,
+   @Description NVARCHAR(MAX),
+   @Price Float,
+   @Rating Float,
+   @ProductId INT OUTPUT
+AS
+
+INSERT Product(SKU, ProductName, ProductTypeId, Quantity, [Description], Price, Rating)
+VALUES(@SKU, @ProductName, @ProductTypeId, @Quantity, @Description, @Price, @Rating);
+
+SET @ProductId = SCOPE_IDENTITY();
+GO
+
+/* Fetch Product Quantity */
+CREATE OR ALTER PROCEDURE Product.FetchProductQuantity
+   @GivenProductID INT
+AS
+
+SELECT 
+	P.ProductName, P.Quantity
+FROM 
+	Product P
+WHERE 
+	P.ProductID = @GivenProductID
+GO
+
+/* Get Product Name */
+CREATE OR ALTER PROCEDURE Product.GetOrderIdName
+   @GivenOrderID INT
+AS
+
+SELECT 
+	OL.OrderID, 
+	P.ProductName, 
+	OL.Quantity 
+FROM 
+	Sales.OrderLines OL
+		INNER JOIN Product P ON P.ProductID = OL.ProductID
+WHERE 
+	OL.OrderID = @GivenOrderID
+GO
+
+/* Get Product Name */
+CREATE OR ALTER PROCEDURE Product.GetProductIdName
+   @GivenProductTypeID INT
+AS
+
+SELECT 
+	P.ProductTypeID,
+	P.ProductName, 
+	P.Quantity
+FROM 
+	Product P
+WHERE 
+	P.ProductTypeID = @GivenProductTypeID
+GO
+
+/* Retrieve Highest Ratings */
+CREATE OR ALTER PROCEDURE Product.RetrieveHighestRatings
+AS
+
+SELECT 
+	P.ProductId, P.SKU, P.ProductName, P.Quantity, P.[Description], P.Price, P.Rating
+FROM 
+	Product P
+ORDER BY 
+	P.Rating DESC
+GO
+
+/* Retrieve Employees */
+CREATE OR ALTER PROCEDURE Sales.RetrieveEmployees
+AS
+
+SELECT E.EmployeeID, E.Email, E.[Name], E.EmployeeRole, E.Salary
+FROM Sales.Employees E;
+GO
+
+/* Retrieve Product Type Count */
+CREATE OR ALTER PROCEDURE Product.RetrieveProductTypeCount
+AS
+
+SELECT PT.[Name], COUNT(O.OrderID) AS OrderCount, P.SKU
+FROM Product.ProductType PT
+    INNER JOIN Product P ON P.ProductTypeID = PT.ProductTypeID
+	INNER JOIN Sales.Orders O ON O.OrderID = P.ProductID
+GROUP BY PT.[Name], P.SKU
+ORDER BY COUNT(P.ProductID) DESC, P.SKU DESC
+GO
+
+/* Fetch Employee Salary */
+CREATE OR ALTER PROCEDURE Sales.FetchEmployeeSalary
+   @GivenEmployeeID INT
+AS
+
+SELECT 
+	E.[Name],
+	E.Salary
+FROM 
+	Sales.Employees E
+WHERE 
+	E.EmployeeID = @GivenEmployeeID
+GO
+
+/* Retrieve Employee Count */
+CREATE OR ALTER PROCEDURE Sales.RetrieveEmployeeCount
+   @GivenRole NVARCHAR(32)
+AS
+
+SELECT E.EmployeeRole, COUNT(EmployeeID) AS SumOfEmployees
+FROM Sales.Employees E
+WHERE E.EmployeeRole = @GivenRole
+GROUP BY E.EmployeeRole
+GO
+
+/* Fetch Member Status */
+CREATE OR ALTER PROCEDURE Users.FetchMemberStatus
+   @GivenMemberid INT
+AS
+
+SELECT M.MemberID, M.[Status]
+FROM Users.Member M
+WHERE M.MemberID = @GivenMemberid
+GROUP BY M.MemberID, M.[Status]
+ORDER BY M.MemberID ASC
+GO
+
+/* Get MemberId */
+CREATE OR ALTER PROCEDURE Users.GetMemberId
+   @GivenEmail NVARCHAR(128)
+AS
+
+SELECT M.MemberID, M.FirstName, M.LastName
+FROM Users.Member M
+WHERE M.Email = @GivenEmail
+GO
+
+/* Get Shipment Address */
+CREATE OR ALTER PROCEDURE Warehouse.GetShipmentAddress
+   @GivenAddress NVARCHAR(128)
+AS
+
+SELECT S.ShipmentID, S.ShipmentAddress, O.OrderID
+FROM Sales.Orders O
+    INNER JOIN Warehouse.Shipment S ON S.ShipmentAddress = O.ShipmentAddress
+WHERE S.ShipmentAddress = @GivenAddress
+GROUP BY S.ShipmentID, S.ShipmentAddress, O.OrderID
+ORDER BY S.ShipmentID ASC
+GO
+
 /* Report #1 */
 SELECT 
 	P.ProductId, 
